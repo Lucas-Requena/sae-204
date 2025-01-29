316475bcc7df40ab2f8c49ee9488fa0c599427b3
lrequena-iut90 <lucas.requena.foch@gmail.com>
2025-01-29 09:43:26 +0100

    test

--- controllers/fixtures_load.py
+++ controllers/fixtures_load.py
@@ -11,29 +11,16 @@ fixtures_load = Blueprint('fixtures_load', __name__,
 @fixtures_load.route('/base/init')
 def fct_fixtures_load():
     mycursor = get_db().cursor()
-    db = get_db()
-    
-    # Suppression des tables dans l'ordre (en respectant les contraintes de clés étrangères)
-    tables_to_drop = [
-        'ligne_commande',
-        'ligne_panier',
-        'commande',
-        'vetement',
-        'type_vetement',
-        'taille',
-        'etat',
-        'utilisateur'
-    ]
-    
-    for table in tables_to_drop:
-        try:
-            sql = f"DROP TABLE IF EXISTS {table}"
-            mycursor.execute(sql)
-            db.commit()
-        except Exception as e:
-            print(f"Erreur lors de la suppression de la table {table}: {str(e)}")
-            continue
+    sql='''DROP TABLE IF EXISTS ligne_commande;
+DROP TABLE IF EXISTS ligne_panier;
+DROP TABLE IF EXISTS vetement;
+DROP TABLE IF EXISTS type_vetement;
+DROP TABLE IF EXISTS taille;
+DROP TABLE IF EXISTS commande ;
+DROP TABLE IF EXISTS etat ;
+DROP TABLE IF EXISTS utilisateur ;   '''
 
+    mycursor.execute(sql)
     sql='''
     CREATE TABLE utilisateur(
    id_utilisateur INT AUTO_INCREMENT,
@@ -72,23 +59,6 @@ INSERT INTO type_vetement (id_type_vetement, libelle_type_vetement) VALUES
                                                                         (4, 'Veste');'''
     mycursor.execute(sql)
 
-    sql='''
-CREATE TABLE taille(
-    id_taille INT AUTO_INCREMENT,
-    libelle_taille VARCHAR(255),
-    PRIMARY KEY (id_taille)
-)DEFAULT CHARSET utf8;'''
-    mycursor.execute(sql)
-
-    sql='''
-INSERT INTO taille (id_taille, libelle_taille) VALUES
-    (1, 'S'),
-    (2, 'M'),
-    (3, 'L'),
-    (4, 'XL'),
-    (5, 'XXL'),
-    (6, 'XXXL');'''
-    mycursor.execute(sql)
 
     sql=''' 
     CREATE TABLE etat(
